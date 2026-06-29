0baced85fb2788021736159ab8659b2c4f7bb70b
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2026-06-29 11:23:40 +0530

    prepared for dev

--- src/main.py
+++ src/main.py
@@ -1,8 +1,5 @@
 def main():
     print("Hello from workdir!")
-    print("Hello from CPU!")
-    print("The change is from master")
-    print("Check after ultralytics")
 
 
 if __name__ == "__main__":
