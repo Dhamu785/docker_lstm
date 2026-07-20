22e540f60f0b9f82e7fc0d08708e369d93043729
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2026-07-20 15:58:40 +0530

    copy

---
+++ src/data_norm.py
@@ -0,0 +1,8 @@
+# %%
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+
+# %% Load dataset
+data = np.load('data/amzn_max.npy')
+data
+# %%
---
+++ src/data_preprocessing.py
@@ -0,0 +1,36 @@
+# %% Import libaries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from copy import deepcopy as dc
+
+# %% device setup
+df = pd.read_csv('data/AMZN_max.csv')
+df.head()
+
+# %% data pre-processsing
+data = df[['Date', 'Close']]
+data.head()
+
+# %% data plotting
+data['Date'] = pd.to_datetime(data["Date"], utc=True).dt.date
+plt.plot(data['Date'], data['Close'])
+
+# %% dataset creation
+def data_for_lstm(df, steps):
+    df = dc(df)
+    df.set_index('Date', inplace=True)
+    for i in range(1, steps+1):
+        df[f'Close(t-{i})'] = df['Close'].shift(i)
+    df.dropna(inplace=True)
+    
+    return df
+
+lookback = 7
+shifted_df = data_for_lstm(data, lookback)
+shifted_df.head()
+# %%
+shifted_df.to_csv('data/lstm_amzn.csv')
+# %%
+np_array = shifted_df.to_numpy()
+np.save('data/amzn_max.npy', np_array)
